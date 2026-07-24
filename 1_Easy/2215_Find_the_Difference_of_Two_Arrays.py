e2a52214f16c94f647da7a56f812f20dc4a85b8f
Vaibhav Singh <78935045+iamvaibhavsingh09@users.noreply.github.com>
2026-07-24 23:50:06 +0530

    LC-2215 Find the Difference of Two Arrays

---
+++ 1_Easy/2215_Find_the_Difference_of_Two_Arrays.py
@@ -0,0 +1,9 @@
+"""
+LeetCode: 2215
+Title: Find the Difference of Two Arrays
+Difficulty: Easy
+
+"""
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        return [list(set(nums1).difference(set(nums2))),list(set(nums2).difference(set(nums1)))]
