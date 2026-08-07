483f9cdb786339b2fbbaff7a1b246f2f653aeb80
Vaibhav Singh <78935045+iamvaibhavsingh09@users.noreply.github.com>
2026-08-07 09:21:53 +0000

    LC-1051 Height Checker

---
+++ 1_Easy/1051_Height_Checker.py
@@ -0,0 +1,17 @@
+"""
+LeetCode: 1051
+Title: Height Checker
+Difficulty: Easy
+
+"""
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        output = 0
+        expected = sorted(heights)
+
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                output += 1
+
+        return output
