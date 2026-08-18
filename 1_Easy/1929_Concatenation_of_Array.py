ff5bb06c61e32c7c6ca26cbbad390c6b3aca5266
Vaibhav Singh <78935045+iamvaibhavsingh09@users.noreply.github.com>
2026-08-18 20:28:43 +0530

    LC-1929 Concatenation of Array

---
+++ 1_Easy/1929_Concatenation_of_Array.py
@@ -0,0 +1,11 @@
+"""
+LeetCode: 1929
+Title: Concatenation of Array
+Difficulty: Easy
+
+"""
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        result = nums + nums
+        return result
