52eacaf6684736b8042eeaa7efc6df45b7236d45
Vaibhav Singh <78935045+iamvaibhavsingh09@users.noreply.github.com>
2026-08-20 07:16:19 +0000

    LC-1662 Check If Two String Arrays are Equivalent

---
+++ 1_Easy/1662_Check_If_Two_String_Arrays_are_Equivalent.py
@@ -0,0 +1,13 @@
+"""
+LeetCode: 1662
+Title: Check If Two String Arrays are Equivalent
+Difficulty: Easy
+
+"""
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        if ''.join(word1) == ''.join(word2):
+            return True
+        else:
+            return False
