915c2bb42a5b24e09224e0d0f933ab8cca233c2d
Vaibhav Singh <78935045+iamvaibhavsingh09@users.noreply.github.com>
2026-08-26 21:40:44 +0530

    LC-3467 Transform Array by Parity
    Implement transformation of array by parity.

---
+++ 1_Easy/3467_Transform_Array_by_Parity.py
@@ -0,0 +1,18 @@
+"""
+LeetCode: 3467
+Title: Transform Array by Parity
+Difficulty: Easy
+
+"""
+
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        result = []
+
+        for num in nums:
+            if num % 2 == 0:
+                result.append(0)
+            else:
+                result.append(1)
+
+        return sorted(result)
